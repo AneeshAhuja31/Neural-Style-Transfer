9a1b64729dad016206c77552af427554e6f5c151
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-02-26 19:27:14 +0530

    more tuning

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def main():
         #try:
           with tf.GradientTape() as tape:
             gamma = max(30 - (i//5),10)
-            loss = compute_total_loss(model, content_img_preprocessed, style_img_preprocessed, generated_img,alpha=0.05, beta=5e3,gamma=gamma)
+            loss = compute_total_loss(model, content_img_preprocessed, style_img_preprocessed, generated_img,alpha=0.01, beta=10e3,gamma=gamma)
           grad = tape.gradient(loss, generated_img)
           optimizer.apply_gradients([(grad, generated_img)])
         
